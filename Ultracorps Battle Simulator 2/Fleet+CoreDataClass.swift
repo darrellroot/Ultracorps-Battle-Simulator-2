e712472cedd378b21240055fb83986e55280ced1
Darrell Root <darrellroot@mac.com>
2018-06-15 11:16:23 -0700

    Pre core data addition

---
+++ Ultracorps Battle Simulator 2/Fleet+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Fleet+CoreDataClass.swift
+//  Ultracorps Battle Simulator 2
+//
+//  Created by Darrell Root on 6/6/18.
+//  Copyright © 2018 com.darrellroot. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Fleet)
+public class Fleet: NSManagedObject {
+
+}
